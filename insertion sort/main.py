196868f8e3d29778377fac10bf05b134defd6cbb
Aluisio Lima <aaluisio309@gmail.com>
2025-03-07 14:39:53 -0300

    update algorithm insertion sorting adding version increasing and decreasing

--- insertion sort/main.py
+++ insertion sort/main.py
@@ -3,11 +3,9 @@
 #      Organize an array
 # ==============================
 
-from typing import List
+array : list[int] = [1,4,53,60,2,7,4,7]
 
-array : list[int] = [1,4,53,6,2,7,4,7]
-
-def insertion_sort(array : list[int]) -> list[int]:
+def insertion_sort_increasing(array : list[int]) -> list[int]:
     for i in range(1, len(array)):
         key = array[i]
         j = i - 1
@@ -22,4 +20,22 @@ def insertion_sort(array : list[int]) -> list[int]:
 
     return array 
 
-print(f"{insertion_sort(array=array)}")
+
+print(f"{insertion_sort_increasing(array=array)}")
+
+def insertion_sort_decreasing(array : list[int]) -> list[int]:
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+
+        # Move elements of array[0..i-1], that are greater than key,
+        # one position ahead of their current position
+        while j >= 0 and array[j] < key:
+            array[j + 1] = array[j]
+            j -= 1
+        
+        array[j + 1] = key  # Insert key at the correct position
+
+    return array 
+
+print(f"{insertion_sort_decreasing(array=array)}")
